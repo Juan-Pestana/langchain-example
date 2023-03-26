7235c9aa020dfcd7d4b4c5144093a20d7b22a9ef
Juan Pestana <juan.pestana82@gmail.com>
2023-03-26 09:34:04 +0200

    added content encoding none

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,46 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
-import { HNSWLib } from "langchain/vectorstores";
-import { OpenAIEmbeddings } from "langchain/embeddings";
-import { makeChain } from "./util";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import path from 'path'
+import { HNSWLib } from 'langchain/vectorstores'
+import { OpenAIEmbeddings } from 'langchain/embeddings'
+import { makeChain } from './util'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const body = req.body;
-  const dir = path.resolve(process.cwd(), "data");
+  const body = req.body
+  const dir = path.resolve(process.cwd(), 'data')
 
-  const vectorstore = await HNSWLib.load(dir, new OpenAIEmbeddings());
+  const vectorstore = await HNSWLib.load(dir, new OpenAIEmbeddings())
   res.writeHead(200, {
-    "Content-Type": "text/event-stream",
+    'Content-Type': 'text/event-stream',
+    //added this to try to fix connection issues
+    'Content-Encoding': 'none',
     // Important to set no-transform to avoid compression, which will delay
     // writing response chunks to the client.
     // See https://github.com/vercel/next.js/issues/9965
-    "Cache-Control": "no-cache, no-transform",
-    Connection: "keep-alive",
-  });
+    'Cache-Control': 'no-cache, no-transform',
+    Connection: 'keep-alive',
+  })
 
   const sendData = (data: string) => {
-    res.write(`data: ${data}\n\n`);
-  };
+    res.write(`data: ${data}\n\n`)
+  }
 
-  sendData(JSON.stringify({ data: "" }));
+  sendData(JSON.stringify({ data: '' }))
   const chain = makeChain(vectorstore, (token: string) => {
-    sendData(JSON.stringify({ data: token }));
-  });
+    sendData(JSON.stringify({ data: token }))
+  })
 
   try {
     await chain.call({
       question: body.question,
       chat_history: body.history,
-    });
+    })
   } catch (err) {
-    console.error(err);
+    console.error(err)
     // Ignore error
   } finally {
-    sendData("[DONE]");
-    res.end();
+    sendData('[DONE]')
+    res.end()
   }
 }
